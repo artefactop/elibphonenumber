c1ef454bfc5fd9d476aa0bee3da89078163d3200
Pepe <pepe@yuilop.com>
2014-01-13 14:35:56 +0100

    Fix file name

--- test/example_test.erl
+++ test/example_test.erl
@@ -3,6 +3,6 @@
 -include_lib("eunit/include/eunit.hrl").
 
 get_country_region_from_number_test() ->
-	PhoneNumber = phonenumber_util:parse(<<"+34644743083">>, <<"">>), 
-	<<"ES">> = phonenumber_util:get_region_code_for_number(PhoneNumber). 
+    PhoneNumber = phonenumber_util:parse(<<"+34644743083">>, <<"">>), 
+    <<"ES">> = phonenumber_util:get_region_code_for_number(PhoneNumber). 
     
